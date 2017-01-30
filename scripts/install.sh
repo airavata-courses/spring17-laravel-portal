161e9a2822d1ffa6783017af42238117606471e9
Sneha Tilak <sneha.tilak26@gmail.com>
2017-01-30 18:03:59 -0500

    Adding logs #6

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,8 +7,8 @@ cd laravel-portal/
 cp .env.example .env
 sudo chmod 777 -R storage/
 sudo chmod 777 .env
-echo 'Logs begin here...' >> /var/log/composer.log 2>&1 &
-pwd >> /var/log/composer.log 2>&1 &
+echo 'Logs begin here...' >> /var/log/composer.log
+pwd >> /var/log/composer.log
 composer install >> /var/log/composer.log 2>&1 &
 php artisan key:generate >> /var/log/composer.log 2>&1 &
 php artisan serve --port=4000 --host=0.0.0.0 & >> /var/log/composer.log 2>&1 &
