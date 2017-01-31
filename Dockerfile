f9eb07679175a12b6a5103d5ce11db99b6bae80b
Sneha Tilak <sneha.tilak26@gmail.com>
2017-01-30 20:03:25 -0500

    Deploying three portals on one instance #6

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
 FROM php:5.6.29-apache
 
 COPY . /var/www/laravel-develop/
-
-EXPOSE 8000
-EXPOSE 8081
--- scripts/install.sh
+++ scripts/install.sh
@@ -9,10 +9,12 @@ cp .env.example .env
 composer install >> /var/log/composer.log 2>&1 &
 sudo chmod -R 777 /home/ec2-user/laravel-portal/
 php artisan key:generate
+php artisan serve --port=3000 --host=0.0.0.0 & >> /var/log/composer.log 2>&1 &
 php artisan serve --port=4000 --host=0.0.0.0 & >> /var/log/composer.log 2>&1 &
+php artisan serve --port=5000 --host=0.0.0.0 & >> /var/log/composer.log 2>&1 &
 
 echo 'Running Docker container'
 sudo docker login -e="sneha.tilak26@gmail.com" -u="tilaks" -p="laravel"
 sudo docker pull tilaks/laravel-portal
 sudo docker images | grep '<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f
-sudo docker run -d -p 4000:8000 --name laravel $(docker images | grep -w "tilaks/laravel-portal" | awk '{print $3}') >> /var/log/laravel.log 2>&1 &
+sudo docker run -d --name laravel $(docker images | grep -w "tilaks/laravel-portal" | awk '{print $3}') >> /var/log/laravel.log 2>&1 &
