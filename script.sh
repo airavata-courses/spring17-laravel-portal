5abb049a80ed7967cab251eb5d9e81a282538667
Thanmai Bindi <thanmai.m17@gmail.com>
2017-02-02 23:04:13 -0500

    Made changes to script #8

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ chmod 777 .env
 
 echo 'Generate the Artisan key'
 php artisan key:generate >> /var/log/script.log 2>&1 &
+php artisan config:clear >> /var/log/script.log 2>&1 &
 
 echo 'Run the portals'
 php artisan serve --port=3000 --host=0.0.0.0 & >> /var/log/script.log 2>&1 &
