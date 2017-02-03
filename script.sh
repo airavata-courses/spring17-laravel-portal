8a51383cfbfd7a2dfdc059724f8c225a24e161b8
Thanmai Bindi <thanmai.m17@gmail.com>
2017-02-03 14:23:36 -0500

    Modified to add sudo permissions #10

--- script.sh
+++ script.sh
@@ -16,8 +16,8 @@ echo 'Change .env permission'
 chmod 777 .env
 
 echo 'Generate the Artisan key'
-php artisan key:generate
 php artisan config:clear
+php artisan key:generate
 
 echo 'Run the portals'
 php -S 0.0.0.0:$PORT server.php >> /var/log/script-$PORT.log
