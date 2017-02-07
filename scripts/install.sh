450223b06e1ad7fc8adc084ed1b2144b380dd8de
Thanmai Bindi <thanmai.m17@gmail.com>
2017-02-06 21:47:55 -0500

    Modified to be non interactive #10

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,9 +10,9 @@ echo 'Running the Docker container'
 sudo docker login -e="sneha.tilak26@gmail.com" -u="tilaks" -p="laravel"
 sudo docker pull tilaks/laravel-portal
 sudo docker images | grep '<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f
-sudo docker run -d --name laravel tilaks/laravel-portal >> /var/log/laravel.log 2>&1
+sudo docker run -p 3000:3000 -p 4000:4000 -p 5000:5000 -d --name laravel tilaks/laravel-portal >> /var/log/laravel.log 2>&1
 
-sudo docker exec -it laravel /bin/bash /var/www/laravel-develop/composer_install.sh >> /var/log/laravel.log 2>&1
-sudo docker exec -it -d laravel php /var/www/laravel-develop/artisan serve --port=3000 --host=0.0.0.0 >> /var/log/laravel.log 2>&1
-sudo docker exec -it -d laravel php /var/www/laravel-develop/artisan serve --port=4000 --host=0.0.0.0 >> /var/log/laravel.log 2>&1
-sudo docker exec -it -d laravel php /var/www/laravel-develop/artisan serve --port=5000 --host=0.0.0.0 >> /var/log/laravel.log 2>&1
+sudo docker exec laravel /bin/bash /var/www/laravel-develop/composer_install.sh >> /var/log/laravel.log 2>&1
+sudo docker exec -d laravel php /var/www/laravel-develop/artisan serve --port=3000 --host=0.0.0.0 >> /var/log/laravel.log 2>&1
+sudo docker exec -d laravel php /var/www/laravel-develop/artisan serve --port=4000 --host=0.0.0.0 >> /var/log/laravel.log 2>&1
+sudo docker exec -d laravel php /var/www/laravel-develop/artisan serve --port=5000 --host=0.0.0.0 >> /var/log/laravel.log 2>&1
